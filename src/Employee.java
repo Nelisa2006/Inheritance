1322873a216f77601f707c79438576cead0371ce
Nelisa2006 <nelisa.sahula@gmail.com>
2020-09-07 22:56:14 +0200

    added getters and setter methods

--- src/Employee.java
+++ src/Employee.java
@@ -1,7 +1,47 @@
 public class Employee {
-    String fullname;
+    String fullnames;
     String email;
     int age;
     Double salary;
     Double numberOfHoursWorked;
+
+    public String getFullnames(){
+        return fullnames;
+    }
+
+    public void setFullnames (String fullnames) {
+        this.fullnames = fullnames;
+    }
+    public String getEmail(){
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public int getAge(){
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public Double getSalary() {
+            return salary;
+        }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public Double getNumberOfHoursWorked(){
+        return numberOfHoursWorked;
+    }
+
+    public void setNumberOfHoursWorked(Double numberOfHoursWorked) {
+        this.numberOfHoursWorked = numberOfHoursWorked;
+    }
 }
+
+
+
